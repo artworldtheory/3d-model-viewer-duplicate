f616989ad0c37e957b6959b834a6ff30bfda619e
artworldtheory <arthur.hopewell@gmail.com>
2024-07-08 15:22:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -70,10 +70,10 @@ function init() {
                 });
 
                 // Position, scale and rotate the second model
-                model2.position.set(0, 5, 0); // Adjusted: Set initial position to the origin and move up slightly
+                model2.position.set(0, 3, 100); // Adjusted: Set initial position to the origin and move up slightly
                 model2.scale.set(150, 150, 150); // Scale down the second model slightly
-                model2.rotation.y = Math.PI / 8; // Rotate slightly towards the viewer
-                model2.rotation.x = -Math.PI / 8; // Rotate downwards slightly
+                model2.rotation.y = Math.PI / 9; // Rotate slightly towards the viewer
+                model2.rotation.x = -Math.PI / -9.5; // Rotate downwards slightly
 
                 scene.add(model2);
 
